afa3856d50dec42c61b6395a007fbdea525ef3d9
Lukas Bosshart <lukas.bosshart@students.unibe.ch>
2023-04-28 18:41:48 +0200

    VM CL run

---
+++ runConfigurations/CL_csv_4.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+java -jar ~/workspace/ddminj/target/ddminj-1.0-SNAPSHOT-jar-with-dependencies.jar generator.CodeLineMWEGenerator ~/workspace/defects4j/bugs/csv_4_b/ src/main/java src/test/java org.apache.commons.csv.CSVParserTest#testNoHeaderMap "java.lang.NullPointerException"
